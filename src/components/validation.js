d80a11df0a94c7a14772076ed48bdbc152f4d2f3
prains <sleskov2013@yandex.ru>
2023-01-16 23:22:27 +0300

    123

--- src/components/validation.js
+++ src/components/validation.js
@@ -62,9 +62,6 @@ export function enableValidation(settings) {
   const formList = Array.from(document.querySelectorAll(settings.formSelector));
 
   formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-    });
     makeValidationListeners(formElement, settings);
   });
 }
